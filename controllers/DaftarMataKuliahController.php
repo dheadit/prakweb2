6d42e8d0da2453a25e5e4f0ff0c9045243ed83a1
dheadit <Andiaditya.b@gmail.com>
2023-05-09 10:49:12 +0800

    Versi pertama web 2

---
+++ controllers/DaftarMataKuliahController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\controllers;
+
+class DaftarMataKuliahController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    public function actionView()
+    {
+        return $this->render('view');
+    }
+    public function actionUpdate()
+    {
+        return $this->render('update');
+    }
+    public function actionDelete()
+    {
+        return $this->render('delete');
+    }
+
+    
+}
